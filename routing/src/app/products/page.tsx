02880a3a78c85000bb9cfffc185b8a13b094ee8c
amgowda.52 <annappagowda@blipsnip.local>
2025-11-03 12:59:31 +0530

    basic Ui routing with Link

--- routing/src/app/products/page.tsx
+++ routing/src/app/products/page.tsx
@@ -1,12 +1,29 @@
+import Link from "next/link";
+
+interface Product {
+  id: number;
+  name: string;
+}
+
 export default function Products() {
+  const product: Product[] = [
+    { id: 1, name: "Product 1" },
+    { id: 2, name: "Product 2" },
+    { id: 3, name: "Product 3" },
+    { id: 4, name: "Product 4" },
+  ];
+
   return (
     <>
       <h1>Products Page</h1>
-      <ol>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-        <li>Product 4</li>
+      <ol >
+        <li className="flex flex-col gap-3">
+          {product.map((pro) => (
+            <Link key={pro.id} href={`/products/${pro.id}`}>
+              Product {pro.id}
+            </Link>
+          ))}
+        </li>
       </ol>
     </>
   );
